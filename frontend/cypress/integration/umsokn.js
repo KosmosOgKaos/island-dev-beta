371e82fccc5956eb41197a834907d1ce94e927ea
arnorhs <arnor@kosmosogkaos.is>
2021-08-17 16:04:34 +0000

    Adding extra test for umsokn authentication (#15)

---
+++ frontend/cypress/integration/umsokn.js
@@ -0,0 +1,23 @@
+describe("Visiting the application page", () => {
+  it('unauthenticated', () => {
+    cy.visit('/umsokn')
+    cy.url().should('match', /login/)
+  })
+
+  it('subpage, unauthenticated', () => {
+    cy.visit('/umsokn/asdf')
+    cy.url().should('match', /login/)
+  })
+
+  it('authenticated', () => {
+    cy.window().then(win => {
+      win.sessionStorage.setItem('user', JSON.stringify({
+        token: 'xxxxxx',
+        user: '0910815209',
+      }))
+
+      cy.visit('/umsokn')
+      cy.url().should('match', /umsokn/)
+    })
+  })
+})
