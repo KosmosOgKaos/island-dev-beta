9a5c1c7653c2e8c443091efd94b4dd466a6a5229
Brian <barabrian89@gmail.com>
2021-08-17 14:07:00 +0000

    d (#21)

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,5 @@
+export { SelectController } from './SelectController'
+
 export { InputController } from './InputController'
 export { VinnumalastofnunLogo } from './VinnumalastofnunLogo'
 export { FormLayout } from './FormLayout'
