bee279a169da087d9a084ab7cb7f6f38040ee905
arnorhs <arnorhs@gmail.com>
2021-08-17 19:36:54 +0000

    calculate

---
+++ frontend/lib/calculate/calculate.spec.ts
@@ -0,0 +1,7 @@
+import { calculate } from './'
+
+describe('calculation', () => {
+  it('should work', () => {
+    expect(calculate(1, 2)).toEqual(3)
+  })
+})
---
+++ frontend/lib/calculate/index.ts
@@ -0,0 +1,23 @@
+export const calculate = ({
+
+}) => {
+  const hlutfallPersAfsl = 1.0
+  const personuafslattur = 50792
+  const tekjurAManudi = 589459
+  const elliOrorkuTSR = 25000
+  const elliOrorkuAlm = 0
+  // Tekjuskattur þrep 1 0-349.018 kr.
+  const tekjuskatturStep1 = 0.3145
+  const tekjuskatturStep2 = 0.3795
+  const vidbodarlifeyrissparnadur = .04
+  const stettarfelagHlutfall = .01
+  const starfshlutfall = 1.0
+  const lifeyrissjodurHlutfall = .04
+  // Grunnatvinnuleysisbætur 100% starfshlutfall
+  const grunnbaetur =
+
+
+
+
+
+}
