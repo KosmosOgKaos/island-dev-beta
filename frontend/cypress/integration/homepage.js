982a90f0bba0977da52a62a6080985f09a7e31e6
arnorhs <arnorhs@gmail.com>
2021-08-17 10:38:18 +0000

    fixing homepage test

--- frontend/cypress/integration/homepage.js
+++ frontend/cypress/integration/homepage.js
@@ -1,6 +1,6 @@
 describe("Renders the home page", () => {
   it('Renders correctly', () => {
     cy.visit('/')
-    cy.get('#main').should('exist')
+    cy.get('#main-content').should('exist')
   })
 })
