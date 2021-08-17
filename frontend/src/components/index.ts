0fccadd1f857e83d840fadfbe95552104619399c
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-08-17 13:50:37 +0000

    Fix conflict

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 export { InputController } from './InputController'
 export { VinnumalastofnunLogo } from './VinnumalastofnunLogo'
-
-export { Button } from './Button'
-=======
->>>>>>> 0183ff4 (Add more form field views)
 export { FormLayout } from './FormLayout'
