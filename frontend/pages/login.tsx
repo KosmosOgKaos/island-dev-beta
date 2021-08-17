532b407a85252fed59f5a9e6a28843ff95735c55
jGeir <58644659+jGeir@users.noreply.github.com>
2021-08-17 14:59:37 +0000

    Frontend/login UI (#17)
    * kennitala validation with Kennitala
    
    * wip validation
    
    * Login layout
    
    * centered
    
    * fix
    
    * removed redundancy
    
    * nl
    
    * redirects

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -2,6 +2,11 @@ import type { NextPage } from 'next'
 import { gql, useMutation } from '@apollo/client'
 import { useForm } from 'react-hook-form'
 import { createLoginStore } from '../lib/loginStore'
+import { Button as Btn, Stack, Input, Header, Text, GridRow } from "@island.is/island-ui/core"
+import { LoginLayout } from '@cmp'
+import kennitala from 'kennitala'
+import { useEffect } from 'react'
+import router from 'next/router'
 
 
 const MUTATION_LOGIN = gql`
@@ -16,9 +21,8 @@ const MUTATION_LOGIN = gql`
 const Login: NextPage = () => {
   const { register, handleSubmit, formState: { errors } } = useForm()
   const [ mutation, { data } ] = useMutation(MUTATION_LOGIN)
-
-  const { login } = createLoginStore()
-
+  const { login, isLoggedIn } = createLoginStore()
+  
   const onSubmit = (data: { username: string }) => {
     mutation({
       variables: {
@@ -29,30 +33,56 @@ const Login: NextPage = () => {
     })
   }
 
+  const hasErrors = (data: { username: string }) => {
+    return checkSocialNumber(data.username)
+  }
 
+  function checkSocialNumber(username) {
+    return kennitala.isPerson(username)
+  }
+
+  useEffect(() => {
+    if(isLoggedIn()) {
+      router.push('/umsokn')
+    }
+  })
   if (data?.login?.token) {
     login({
       username: data.login.username,
       token: data.login.token,
     })
 
+
     return null
   }
 
   return (
-    <div>
-      <h1 className="title">Innskráning</h1>
+    <LoginLayout>
+      <div>
+        <GridRow align="center">
+          <Header />
+        </GridRow>
+        <Stack space={3}>
+        <GridRow align="center">
+          <Text variant="h1" as="h1" marginBottom={0}>Skráðu þig inn</Text>
+        </GridRow>
+        <GridRow align="center">
+          <Text variant="h4" as="h4" marginBottom={2}>á mínar síður Ísland.is</Text>
+        </GridRow>
+        </Stack>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <p>Stimplaðu inn kennitölu til þess að skrá þig inn.</p>
-        <input {...register("username", { required: true, minLength: 10, maxLength: 10, pattern: /^-?[0-9]\d*\.?\d*$/ })} />
-
-        <p>{errors.username && <span>Kennitala er ekki gild</span>}</p>
-
-        <input type="submit" value="Auðkenna" />
-      </form>
-    </div>
+        <GridRow align="center">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Text marginBottom={2}>Stimplaðu inn kennitölu til þess að skrá þig inn.</Text>
+            <Input name='username' label="Kennitala" defaultValue="" required {...register("username")} hasError={!hasErrors} />
+            
+            <GridRow align="center" marginTop={2}>
+              <Btn type="submit">Audkenna</Btn>
+            </GridRow>
+          </form>
+        </GridRow>
+      </div>
+    </LoginLayout>
   )
 }
 
