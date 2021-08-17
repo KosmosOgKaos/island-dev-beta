82d5622d9ad73d68cf9de1f46d40f1312eff175d
Kristján Albert <kal@kosmosogkaos.is>
2021-08-17 16:35:03 +0000

    fix build (#29)

--- frontend/src/components/DatePickerController/DatePickerController.tsx
+++ frontend/src/components/DatePickerController/DatePickerController.tsx
@@ -65,7 +65,7 @@ export const DatePickerController = ({
           disabled={disabled}
           size={size}
           id={id}
-          required
+          required={required}
           errorMessage={error?.message}
           locale={locale}
           label={label}
