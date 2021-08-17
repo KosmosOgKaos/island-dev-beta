fbbdd514b2cbbc5e9fc1c0e44bb94f5512dc68b5
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-08-17 10:38:58 +0000

    Add more form field views

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 export { InputController } from './InputController'
 export { VinnumalastofnunLogo } from './VinnumalastofnunLogo'
 
 export { Button } from './Button'
+=======
+>>>>>>> 0183ff4 (Add more form field views)
 export { FormLayout } from './FormLayout'
