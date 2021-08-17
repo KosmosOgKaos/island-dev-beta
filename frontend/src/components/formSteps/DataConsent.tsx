1f76be85b9e40078079290718cd03b109360f932
Kristján Albert <kal@kosmosogkaos.is>
2021-08-17 13:52:23 +0000

    More views (#20)
    * Add more form field views
    
    * Fix conflict

--- frontend/src/components/formSteps/DataConsent.tsx
+++ frontend/src/components/formSteps/DataConsent.tsx
@@ -1,9 +1,65 @@
 import React from 'react'
-import { Box, Text } from '@island.is/island-ui/core'
+import { Box, Checkbox, Icon, Inline, Text } from '@island.is/island-ui/core'
 import { ActiveStepComponentProps } from '../ActiveStep'
 
 export const DataConsent = ({ options, form }: ActiveStepComponentProps) => (
   <Box>
-    <Text variant='h3'>Gagnaöflun</Text>
+    <Text variant="h2" marginBottom={3}>
+      Gagnaöflun
+    </Text>
+
+    <Text variant="h4" marginBottom={6}>
+      <Inline space={1} alignY="center">
+        <Icon color="blue400" type="outline" size="large" icon="download" />{' '}
+        Eftirfarandi gögn verða sótt rafrænt
+      </Inline>
+    </Text>
+
+    <Text variant="h5" color="blue400">
+      Persónuupplýsingar úr Þjóðskrá
+    </Text>
+    <Text marginBottom={4}>
+      Til þess að auðvelda fyrir sækjum við persónuupplýsingar úr Þjóðskrá til
+      þess að fylla út umsóknina
+    </Text>
+
+    <Text variant="h5" color="blue400">
+      Netfang og símanúmer úr þínum stillingum
+    </Text>
+    <Text marginBottom={4}>
+      Sótt eru gögn um menntun, símanúmer og tölvupóstfang af Mínum síðum
+      Ísland.is
+    </Text>
+
+    <Text variant="h5" color="blue400">
+      Tekjuupplýsingar frá Ríkisskattstjóra
+    </Text>
+    <Text marginBottom={4}>
+      Sóttar eru staðgreiðsluupplýsingar til upplýsinga um laun síðustu 12
+      mánuði
+    </Text>
+
+    <Text variant="h5" color="blue400">
+      Elli- og örorkulífeyrisgreiðslur frá Tryggingastofnun og almennum
+      lífeyrissjóðum
+    </Text>
+    <Text marginBottom={4}>
+      Sóttar eru upplýsingar um greiðslur frá Tryggingastofnun og almennum
+      lífeyrissjóðum til grundvallar útreikningi á bótarétti
+    </Text>
+
+    <Text variant="h5" color="blue400">
+      Ökuskírteinaskrá
+    </Text>
+    <Text marginBottom={7}>Sóttar eru upplýsingar um ökuréttindi</Text>
+
+    <Box marginBottom={7}>
+      <Checkbox
+        label="Ég hef kynnt mér ofangreint"
+        labelVariant="default"
+        backgroundColor="blue"
+        large
+      />
+    </Box>
   </Box>
 )
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,3 @@
 export { InputController } from './InputController'
 export { VinnumalastofnunLogo } from './VinnumalastofnunLogo'
-
-export { Button } from './Button'
 export { FormLayout } from './FormLayout'
